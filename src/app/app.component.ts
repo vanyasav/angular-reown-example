ddfe915e8402d2cf2cb3073b4bc0e5dc037d5391
vanyasav <i.savilov@greengreystudio.com>
2024-10-04 10:54:33 +0200

    init reown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { createAppKit } from '@reown/appkit'
+import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
 
 @Component({
   selector: 'app-root',
@@ -8,6 +11,38 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-reown-example';
+
+  ngOnInit() {
+    // 1. Get a project ID at https://cloud.reown.com
+    const projectId = 'YOUR_PROJECT_ID'
+
+    const networks = [mainnet, arbitrum]
+
+    // 2. Set up Wagmi adapter
+    const wagmiAdapter = new WagmiAdapter({
+      projectId,
+      networks
+    })
+
+    // 3. Configure the metadata
+    const metadata = {
+      name: 'AppKit',
+      description: 'AppKit Example',
+      url: 'https://example.com', // origin must match your domain & subdomain
+      icons: ['https://avatars.githubusercontent.com/u/179229932']
+    }
+
+    // 3. Create the modal
+    createAppKit({
+      adapters: [wagmiAdapter],
+      networks: [mainnet, arbitrum],
+      metadata,
+      projectId,
+      features: {
+        analytics: true // Optional - defaults to your Cloud configuration
+      }
+    })
+  }
 }
